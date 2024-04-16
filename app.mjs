98ea6965704b865cfe2df4144479cc8f6f87a494
Sergio K <122477416+Sknow1112@users.noreply.github.com>
2024-04-16 18:13:07 +0000

    module

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import { client } from "@gradio/client";
 
 const app = await client("https://b05029ad640abbade7.gradio.live/");
 const result = await app.predict("/chat", [		
-				"Hello!!", // string  in 'Message' Textbox component
+				"Say hello world.", // string  in 'Message' Textbox component
 	]);
 
 console.log(result.data);
